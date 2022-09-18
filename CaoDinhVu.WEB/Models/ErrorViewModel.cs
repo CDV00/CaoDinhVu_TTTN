198baac3b4c8d44a52a6a841217f972b3288e5ca
CDV00 <caodinhvu00@gmail.com>
2022-09-18 22:36:11 +0700

    first commit

---
+++ CaoDinhVu.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CaoDinhVu.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
